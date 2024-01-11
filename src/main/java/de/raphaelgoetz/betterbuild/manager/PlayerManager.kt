ceb90fc631a973d4e5634eade87ef8f5a45761fc
konfuzius <raphael.goetz.7@gmail.com>
2024-01-11 20:02:37 +0100

    feat: add PlayerManager to toggle build/noClip-Mode

---
+++ src/main/java/de/raphaelgoetz/betterbuild/manager/PlayerManager.kt
@@ -0,0 +1,70 @@
+package de.raphaelgoetz.betterbuild.manager
+
+import org.bukkit.Location
+import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+class PlayerManager {
+
+    private val buildMode: MutableCollection<Player> = ArrayList()
+    private val noClipMode: MutableCollection<Player> = ArrayList()
+    private val lastPlayerLocation: MutableMap<Player, Location> = HashMap()
+
+    fun getLastLocation(player: Player): Location? {
+        return lastPlayerLocation[player]
+    }
+
+    fun toggleBuildMode(player: Player) {
+
+        if (player.isActiveBuilder()) {
+            player.sendMessage("You have been removed from buildMode")
+            buildMode.remove(player)
+            return
+        }
+
+        player.sendMessage("Add to BuildMode")
+        buildMode.add(player)
+    }
+
+    fun toggleNoClipMode(player: Player) {
+
+        if (player.isActiveNoClip()) {
+            player.sendMessage("You have been removed from noClipMode")
+            noClipMode.remove(player)
+            return
+        }
+
+        player.sendMessage("Add to noClipMode")
+        noClipMode.add(player)
+    }
+
+    fun toggleNightVision(player: Player) {
+        if (player.hasActiveNightVision()) player.removePotionEffect(PotionEffectType.NIGHT_VISION)
+        else player.addPotionEffect(PotionEffect(PotionEffectType.NIGHT_VISION, PotionEffect.INFINITE_DURATION, 1, false, false, false))
+    }
+
+    fun cancelWhenBuilder(player: Player, event: Cancellable) {
+        event.isCancelled = player.isActiveBuilder()
+    }
+
+    private fun Player.isActiveBuilder(): Boolean {
+        return buildMode.contains(player)
+    }
+
+    private fun Player.isActiveNoClip(): Boolean {
+        return noClipMode.contains(player)
+    }
+
+    private fun Player.hasActiveNightVision(): Boolean {
+
+        for (potions in player?.activePotionEffects!!) {
+
+            if (potions.type != PotionEffectType.NIGHT_VISION) continue
+            return true
+        }
+
+        return false
+    }
+}
