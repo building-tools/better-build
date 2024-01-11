b672cbe4664bc42905a709a2b55eeee27ce44cf8
konfuzius <raphael.goetz.7@gmail.com>
2024-01-11 20:02:57 +0100

    feat: add VoidGenerator to create empty worlds

---
+++ src/main/java/de/raphaelgoetz/betterbuild/util/VoidGenerator.kt
@@ -0,0 +1,18 @@
+package de.raphaelgoetz.betterbuild.util
+
+import org.bukkit.Material
+import org.bukkit.generator.ChunkGenerator
+import org.bukkit.generator.WorldInfo
+import java.util.*
+
+class VoidGenerator : ChunkGenerator() {
+
+    override fun generateBedrock(worldInfo: WorldInfo, random: Random, chunkX: Int, chunkZ: Int, chunkData: ChunkData) {
+
+        val x = 0; val y = 64; val z = 0
+
+        if (x >= chunkX * 16 && (x < (chunkX + 1) * 16)) return
+        if (!(z >= chunkZ * 16) && (z < (chunkZ + 1) * 16)) return
+        chunkData.setBlock(x, y, z, Material.BEDROCK);
+    }
+}
