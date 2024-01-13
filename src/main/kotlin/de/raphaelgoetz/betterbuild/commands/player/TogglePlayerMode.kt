a667dd3c1a816b0639be88463a345b0c77bb0e77
konfuzius <raphael.goetz.7@gmail.com>
2024-01-13 21:52:31 +0100

    feat: command to toggle your walk and fly speed

---
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/commands/player/TogglePlayerMode.kt
@@ -0,0 +1,28 @@
+package de.raphaelgoetz.betterbuild.commands.player
+
+import de.raphaelgoetz.betterbuild.BetterBuild
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+
+data class TogglePlayerMode(val betterBuild: BetterBuild) : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+
+        if (sender !is Player) return true
+        if (args?.size!! > 1) return true
+
+        val target = if (args.size == 1) Bukkit.getPlayer(args[0]) else sender
+
+        if (target == null) return true
+        if (!target.isOnline) return true
+
+        if ("build" == label) betterBuild.playerManager.toggleBuildMode(target)
+        if ("clip" == label) betterBuild.playerManager.toggleNoClipMode(target)
+
+        return false
+    }
+}
