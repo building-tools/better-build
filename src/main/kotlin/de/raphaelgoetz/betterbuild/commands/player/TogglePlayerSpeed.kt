3182433410c49de2c632cd4a78141d5ef31eecaa
konfuzius <raphael.goetz.7@gmail.com>
2024-01-13 22:08:31 +0100

    fix: removed debugging messages

--- src/main/kotlin/de/raphaelgoetz/betterbuild/commands/player/TogglePlayerSpeed.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/commands/player/TogglePlayerSpeed.kt
@@ -11,15 +11,9 @@ data class TogglePlayerSpeed(val betterBuild: BetterBuild) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
 
-        println(1)
         if (sender !is Player) return true
-        println(11)
-
         if (args?.size!! != 1) return true
-        println(2)
-
         val values = (0 until 11).map { it.toString() }
-        println(3)
 
         if (!values.contains(args[0])) {
             sender.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#EB3349:#F45C43>⚠ Only values from 0 to 10!"));
@@ -27,8 +21,8 @@ data class TogglePlayerSpeed(val betterBuild: BetterBuild) : CommandExecutor {
         }
 
         val speed = ((args[0]).toFloat() / 10);
-
         sender.sendMessage(MiniMessage.miniMessage().deserialize("<gradient:#a8ff78:#78ffd6>✔ Speed updated to: $speed"));
+
         if ("fly" == label) sender.flySpeed = speed;
         if ("walk" == label) sender.walkSpeed = speed;
         return true
