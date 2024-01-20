8277ea5a5b1388e25b4f846d7704a844caebd32e
konfuzius <raphael.goetz.7@gmail.com>
2024-01-20 14:26:47 +0100

    feat: add anvil-inventory to set a world-name

---
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/menus/WorldNameMenu.kt
@@ -0,0 +1,39 @@
+package de.raphaelgoetz.betterbuild.menus
+
+import de.raphaelgoetz.betterbuild.utils.BukkitAnvilInventory
+import de.raphaelgoetz.betterbuild.utils.BukkitAnvilInventorySlots
+import de.raphaelgoetz.betterbuild.utils.ItemBuilder
+import net.kyori.adventure.text.Component
+import org.bukkit.Material
+import org.bukkit.entity.Player
+
+class WorldNameMenu(
+
+    val player: Player,
+    val title: Component
+
+) : BukkitAnvilInventory(title) {
+
+    init {
+        setItems()
+    }
+
+    fun open() {
+        this.open(player)
+    }
+
+    private fun setItems() {
+
+        this.setSlot(BukkitAnvilInventorySlots.FIRST, ItemBuilder(Material.RED_DYE).setName("Cancel").build(), consumer = {
+            it.isCancelled = true
+            player.closeInventory()
+            player.sendMessage("canceled")
+        })
+
+        this.setSlot(BukkitAnvilInventorySlots.SECOND, ItemBuilder(Material.GREEN_DYE).build(), consumer = {
+            it.isCancelled = true
+            player.closeInventory()
+            player.sendMessage(this.getText())
+        })
+    }
+}
