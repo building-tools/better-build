b572dfa1957660471fdf791c6db0d158ca11b581
konfuzius <raphael.goetz.7@gmail.com>
2024-01-26 23:49:38 +0100

    feat: added shortcut to switch game-modes

---
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/commands/player/TogglePlayerGamemode.kt
@@ -0,0 +1,42 @@
+package de.raphaelgoetz.betterbuild.commands.player
+
+import de.raphaelgoetz.betterbuild.BetterBuild
+import org.bukkit.GameMode
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+data class TogglePlayerGamemode(val betterBuild: BetterBuild) : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+
+        if (sender !is Player) return true
+        if (args == null) return true
+
+        if (!sender.hasPermission("betterbuild.player.gamemode")) {
+            betterBuild.languageManager.sendPlayerMessage(sender, "command.player.gamemode.permission")
+            return true
+        }
+
+        if (args.size != 1) {
+            betterBuild.languageManager.sendPlayerMessage(sender, "command.player.gamemode.missing")
+            return true
+        }
+
+        val value = args[0].toInt()
+        if (value > 3) {
+            betterBuild.languageManager.sendPlayerMessage(sender, "command.player.gamemode.error")
+            return true
+        }
+
+        when (value) {
+            0 -> sender.gameMode = GameMode.SURVIVAL
+            1 -> sender.gameMode = GameMode.CREATIVE
+            2 -> sender.gameMode = GameMode.ADVENTURE
+            3 -> sender.gameMode = GameMode.SPECTATOR
+        }
+
+        return false
+    }
+}
