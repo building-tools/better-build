019637149b6d50fdacc16766eb2913ce44df684f
konfuzius <raphael.goetz.7@gmail.com>
2024-01-27 17:04:14 +0100

    feat: added world-creation option through inventory

---
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/listeners/player/PlayerAsyncChatListener.kt
@@ -0,0 +1,29 @@
+package de.raphaelgoetz.betterbuild.listeners.player
+
+import de.raphaelgoetz.betterbuild.BetterBuild
+import io.papermc.paper.event.player.AsyncChatEvent
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+
+data class PlayerAsyncChatListener(val betterBuild: BetterBuild) : Listener {
+
+    @EventHandler
+    fun onPlayerAsyncChatEvent(playerAsyncChatEvent: AsyncChatEvent) {
+
+        val uuid = playerAsyncChatEvent.player.uniqueId
+        if (!betterBuild.worldManager.worldCreation.contains(uuid)) return
+        val buildWorld = betterBuild.worldManager.worldCreation[uuid] ?: return
+        val message = playerAsyncChatEvent.message().toString()
+        println(message)
+
+        buildWorld.name = message
+        Bukkit.getScheduler().runTask(betterBuild, Runnable {
+            betterBuild.worldManager.generateWorld(buildWorld)
+        })
+
+        betterBuild.languageManager.sendPlayerMessage(playerAsyncChatEvent.player, "message.world.created")
+        betterBuild.worldManager.worldCreation.remove(uuid)
+        playerAsyncChatEvent.isCancelled = true
+    }
+}
