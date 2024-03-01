f6ef616ac5aae22afa89de28fded68d424d59f76
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-01 21:41:49 +0100

    feat: added links for skulls

---
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/utils/SkullURL.kt
@@ -0,0 +1,15 @@
+package de.raphaelgoetz.betterbuild.utils
+
+enum class SkullURL(val url: String) {
+    ITEM_CATEGORY("https://textures.minecraft.net/texture/56330a4a22ff55871fc8c618e421a37733ac1dcab9c8e1a4bb73ae645a4a4e"),
+    ITEM_WORLD_1("https://textures.minecraft.net/texture/5a9d914a12c17cccb55899285a066902ba53976807407fcb8696dbe19aef77"),
+    ITEM_WORLD_2("https://textures.minecraft.net/texture/6ded84393bec253cca66c04316726a858e574712b6ff9f273668da98efcc28f"),
+    ITEM_WORLD_3("https://textures.minecraft.net/texture/b1dd4fe4a429abd665dfdb3e21321d6efa6a6b5e7b956db9c5d59c9efab25"),
+    ITEM_WORLD_4("https://textures.minecraft.net/texture/2ee06321bcca58eadc78aeafef188bb613a2a82c3c27ff22aef45c4df2b3d8"),
+    ITEM_WORLD_5("https://textures.minecraft.net/texture/43ed368821b466d39bdf57e78fc1ddf75ec966c16955e02e5948be7adfd1fe55 "),
+    GUI_SPAWN("https://textures.minecraft.net/texture/88a1471f37a362dce3582d570e1ba72705c3b42615c8a5a6f1fa6655a1d1f09"),
+    GUI_WORLD("https://textures.minecraft.net/texture/140d4e3146c06157c4837106212157cd6600b6ff934af933b1dcab88a7c1efa2"),
+    GUI_BACK("https://textures.minecraft.net/texture/44f7bc1fa8217b18b323af841372a3f7c602a435c828faa403d176c6b37b605b"),
+    GUI_CLOSE("https://textures.minecraft.net/texture/e6f1898f1e84805694544944f8b49c7007622a2d9b2bb59a278519a68991ac69"),
+    GUI_ARCHIVE("https://textures.minecraft.net/texture/6612fbceabcc4817657611da1dd0ff281e0ffb304095af69a1f882f1d83c01cb")
+}
