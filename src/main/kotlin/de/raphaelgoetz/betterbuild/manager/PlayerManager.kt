0fade530bb9cf25ff6c7c7d7208b692449fa2ad4
Raphael <raphael.goetz.7@gmail.com>
2024-03-25 21:46:32 +0100

    feat: replaced player with uuid to better handle long server uptimes

--- src/main/kotlin/de/raphaelgoetz/betterbuild/manager/PlayerManager.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/manager/PlayerManager.kt
@@ -1,51 +1,55 @@
 package de.raphaelgoetz.betterbuild.manager
 
-import  de.raphaelgoetz.betterbuild.BetterBuild
+import de.raphaelgoetz.betterbuild.world.BuildWorld
 import org.bukkit.Location
 import org.bukkit.entity.Player
 import org.bukkit.event.Cancellable
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import java.util.UUID
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class PlayerManager(
 
-    private val betterBuild: BetterBuild,
-    private val buildMode: MutableCollection<Player> = ArrayList(),
-    private val noClipMode: MutableCollection<Player> = ArrayList(),
-    private val lastPlayerLocation: MutableMap<Player, Location> = HashMap()
+    private val buildMode: MutableCollection<UUID> = ArrayList(),
+    private val ghostMode: MutableCollection<UUID> = ArrayList(),
+    private val noClipMode: MutableCollection<UUID> = ArrayList(),
+    private val lastPlayerLocation: MutableMap<UUID, Location> = HashMap(),
+    private val worldCreation: MutableMap<UUID, BuildWorld> = mutableMapOf()
 
 ) {
 
     fun getLastLocation(player: Player): Location? {
-        return lastPlayerLocation[player]
+        return lastPlayerLocation[player.uniqueId]
     }
 
     fun setLastLocation(player: Player, location: Location) {
-        this.lastPlayerLocation[player] = location
+        this.lastPlayerLocation[player.uniqueId] = location
     }
 
     fun toggleBuildMode(player: Player) {
 
         if (isActiveBuilder(player)) {
             LanguageManager.sendPlayerMessage(player, "manager.player.build.remove")
-            buildMode.remove(player)
+            buildMode.remove(player.uniqueId)
             return
         }
 
         LanguageManager.sendPlayerMessage(player, "manager.player.build.add")
-        buildMode.add(player)
+        buildMode.add(player.uniqueId)
     }
 
     fun toggleNoClipMode(player: Player) {
 
         if (isActiveNoClip(player)) {
             LanguageManager.sendPlayerMessage(player, "manager.player.clip.remove")
-            noClipMode.remove(player)
+            noClipMode.remove(player.uniqueId)
             return
         }
 
         LanguageManager.sendPlayerMessage(player, "manager.player.clip.add")
-        noClipMode.add(player)
+        noClipMode.add(player.uniqueId)
     }
 
     fun toggleNightVision(player: Player) {
@@ -68,16 +72,31 @@ class PlayerManager(
         LanguageManager.sendPlayerMessage(player, "gui.main.item.night.enable.message")
     }
 
+    fun toggleGhostMode(player: Player) {
+        if (isActiveGhost(player)) {
+            LanguageManager.sendPlayerMessage(player, "manager.player.ghost.remove")
+            ghostMode.remove(player.uniqueId)
+            return
+        }
+
+        LanguageManager.sendPlayerMessage(player, "manager.player.ghost.add")
+        ghostMode.add(player.uniqueId)
+    }
+
     fun cancelWhenBuilder(player: Player, event: Cancellable) {
         event.isCancelled = !isActiveBuilder(player)
     }
 
     fun isActiveBuilder(player: Player): Boolean {
-        return buildMode.contains(player)
+        return buildMode.contains(player.uniqueId)
     }
 
     fun isActiveNoClip(player: Player): Boolean {
-        return noClipMode.contains(player)
+        return noClipMode.contains(player.uniqueId)
+    }
+
+    fun isActiveGhost(player: Player): Boolean {
+        return ghostMode.contains(player.uniqueId)
     }
 
     fun hasActiveNightVision(player: Player): Boolean {
@@ -89,5 +108,12 @@ class PlayerManager(
         }
 
         return false
+    }
+
+    fun clearPlayer(player: Player) {
+        lastPlayerLocation.remove(player.uniqueId)
+        buildMode.remove(player.uniqueId)
+        noClipMode.remove(player.uniqueId)
+        worldCreation.remove(player.uniqueId)
     }
 }
