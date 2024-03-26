6baced1a3782d8b4cf80444d8ccab98bb112da8f
Raphael <raphael.goetz.7@gmail.com>
2024-03-26 20:48:21 +0100

    fix: added stop method

--- src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/Task.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/Task.kt
@@ -1,13 +1,15 @@
 package de.raphaelgoetz.betterbuild.utils.tasks
 
 import org.bukkit.entity.Player
-import org.bukkit.scheduler.BukkitTask
 
 interface Task {
 
     fun getTaskHolder(): Player
 
-    fun getTask(): BukkitTask
+    fun getTask(): Runnable
 
     fun run()
+
+    fun stop()
+
 }
