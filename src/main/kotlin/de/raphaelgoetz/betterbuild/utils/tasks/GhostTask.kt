9cb0d5622beb5af5a02f208433e99cfb1d2c0ac7
Raphael <raphael.goetz.7@gmail.com>
2024-03-26 20:48:43 +0100

    feat: implemented ghost & noClip task

--- src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/GhostTask.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/GhostTask.kt
@@ -1,19 +1,56 @@
 package de.raphaelgoetz.betterbuild.utils.tasks
 
+import de.raphaelgoetz.betterbuild.BetterBuild
+import org.bukkit.Bukkit
+import org.bukkit.entity.BlockDisplay
+import org.bukkit.entity.Interaction
 import org.bukkit.entity.Player
-import org.bukkit.scheduler.BukkitTask
 
-class GhostTask: Task {
+class GhostTask(
+        val betterBuild: BetterBuild,
+        val player: Player,
+) : Task {
+
+    private var taskId: Int = 0;
+    private val actions: MutableMap<BlockDisplay, Interaction> = mutableMapOf()
 
     override fun getTaskHolder(): Player {
-        TODO("Not yet implemented")
+        return player
     }
 
-    override fun getTask(): BukkitTask {
-        TODO("Not yet implemented")
+    override fun getTask(): Runnable {
+        return Runnable {
+            for (entity in player.getNearbyEntities(20.0, 20.0, 20.0)) {
+                if (entity is BlockDisplay) setupInteraction(entity)
+            }
+        }
     }
 
     override fun run() {
-        TODO("Not yet implemented")
+        val task = Bukkit.getScheduler().runTaskTimerAsynchronously(betterBuild, getTask(), 20, 20)
+        taskId = task.taskId
+    }
+
+    override fun stop() {
+        if (taskId == 0) return
+        Bukkit.getScheduler().cancelTask(taskId)
+        actions.forEach { actions ->
+            actions.value.remove()
+        }
+    }
+
+    private fun setupInteraction(entity: BlockDisplay) {
+        if (actions[entity] == null) {
+            val interaction = entity.world.spawn(entity.location, Interaction::class.java)
+
+            interaction.isGlowing = true
+            interaction.interactionHeight = 1.0f
+            interaction.interactionWidth = 1.0f
+
+            for (onlinePlayer in Bukkit.getOnlinePlayers()) {
+                if (onlinePlayer == player) continue
+                onlinePlayer.hideEntity(betterBuild, interaction)
+            }
+        }
     }
 }
--- src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/NoClipTask.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/utils/tasks/NoClipTask.kt
@@ -1,19 +1,59 @@
 package de.raphaelgoetz.betterbuild.utils.tasks
 
+import de.raphaelgoetz.betterbuild.BetterBuild
+import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
-import org.bukkit.scheduler.BukkitTask
+import org.bukkit.util.Vector
 
-class NoClipTask : Task{
+class NoClipTask(
+        val betterBuild: BetterBuild,
+        val player: Player
+) : Task {
+
+    private var taskId = 0
 
     override fun getTaskHolder(): Player {
-        TODO("Not yet implemented")
+        return player
     }
 
-    override fun getTask(): BukkitTask {
-        TODO("Not yet implemented")
+    override fun getTask(): Runnable {
+        return Runnable {
+            if (!player.isSprinting) return@Runnable
+            val location = player.location.add(getOffset(player.facing))
+
+            if (player.gameMode == GameMode.CREATIVE) {
+                if (location.block.isSolid) player.gameMode = GameMode.SPECTATOR
+                return@Runnable
+            }
+
+            if (player.gameMode == GameMode.SPECTATOR) {
+                if (!location.block.isSolid) player.gameMode = GameMode.CREATIVE
+                return@Runnable
+            }
+
+            stop()
+        }
     }
 
     override fun run() {
-        TODO("Not yet implemented")
+        val task = Bukkit.getScheduler().runTaskTimerAsynchronously(betterBuild, getTask(), 20, 20)
+        taskId = task.taskId
+    }
+
+    override fun stop() {
+        if (taskId == 0) return
+        Bukkit.getScheduler().cancelTask(taskId)
+    }
+
+    private fun getOffset(face: BlockFace): Vector {
+        return when(face) {
+            BlockFace.NORTH -> Vector(0.0, 0.0, -0.2)
+            BlockFace.SOUTH -> Vector(0.0, 0.0, 0.2)
+            BlockFace.WEST -> Vector(-0.2, 0.0, 0.0)
+            BlockFace.EAST -> Vector(0.2, 0.0, 0.0)
+            else -> Vector(0, 0, 0)
+        }
     }
 }
