0e08bffad117fb8896c676ef69bef40cc64b1dcb
Raphael <raphael.goetz.7@gmail.com>
2024-05-24 20:11:03 +0200

    feat: added value for list of banner-colors

--- src/main/kotlin/de/raphaelgoetz/betterbuild/utils/BannerColors.kt
+++ src/main/kotlin/de/raphaelgoetz/betterbuild/utils/BannerColors.kt
@@ -1,24 +1,28 @@
 package de.raphaelgoetz.betterbuild.utils
 
+import de.raphaelgoetz.astralis.items.basicItemWithoutMeta
 import org.bukkit.DyeColor
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 enum class BannerColors(val dyeColor: DyeColor, val itemStack: ItemStack, val banner: ItemStack) {
-    WHITE(DyeColor.WHITE, ItemBuilder(Material.WHITE_DYE).build(), ItemBuilder(Material.WHITE_BANNER).build()),
-    ORANGE(DyeColor.ORANGE, ItemBuilder(Material.ORANGE_DYE).build(), ItemBuilder(Material.ORANGE_BANNER).build()),
-    MAGENTA(DyeColor.MAGENTA, ItemBuilder(Material.MAGENTA_DYE).build(), ItemBuilder(Material.MAGENTA_BANNER).build()),
-    LIGHT_BLUE(DyeColor.LIGHT_BLUE, ItemBuilder(Material.LIGHT_BLUE_DYE).build(), ItemBuilder(Material.LIGHT_BLUE_BANNER).build()),
-    YELLOW(DyeColor.YELLOW, ItemBuilder(Material.YELLOW_DYE).build(), ItemBuilder(Material.YELLOW_BANNER).build()),
-    LIME(DyeColor.LIME, ItemBuilder(Material.LIME_DYE).build(), ItemBuilder(Material.LIME_BANNER).build()),
-    PINK(DyeColor.PINK, ItemBuilder(Material.PINK_DYE).build(), ItemBuilder(Material.PINK_BANNER).build()),
-    GRAY(DyeColor.GRAY, ItemBuilder(Material.GRAY_DYE).build(), ItemBuilder(Material.GRAY_BANNER).build()),
-    LIGHT_GRAY(DyeColor.LIGHT_GRAY, ItemBuilder(Material.LIGHT_GRAY_DYE).build(), ItemBuilder(Material.LIGHT_GRAY_BANNER).build()),
-    CYAN(DyeColor.CYAN, ItemBuilder(Material.CYAN_DYE).build(), ItemBuilder(Material.CYAN_BANNER).build()),
-    PURPLE(DyeColor.PURPLE, ItemBuilder(Material.PURPLE_DYE).build(), ItemBuilder(Material.PURPLE_BANNER).build()),
-    BLUE(DyeColor.BLUE, ItemBuilder(Material.BLUE_DYE).build(), ItemBuilder(Material.BLUE_BANNER).build()),
-    BROWN(DyeColor.BROWN, ItemBuilder(Material.BROWN_DYE).build(), ItemBuilder(Material.BROWN_BANNER).build()),
-    GREEN(DyeColor.GREEN, ItemBuilder(Material.GREEN_DYE).build(), ItemBuilder(Material.GREEN_BANNER).build()),
-    RED(DyeColor.RED, ItemBuilder(Material.RED_DYE).build(), ItemBuilder(Material.RED_BANNER).build()),
-    BLACK(DyeColor.BLACK, ItemBuilder(Material.BLACK_DYE).build(), ItemBuilder(Material.BLACK_BANNER).build())
+    WHITE(DyeColor.WHITE, basicItemWithoutMeta(Material.WHITE_DYE), basicItemWithoutMeta(Material.WHITE_BANNER)),
+    ORANGE(DyeColor.ORANGE, basicItemWithoutMeta(Material.ORANGE_DYE), basicItemWithoutMeta(Material.ORANGE_BANNER)),
+    MAGENTA(DyeColor.MAGENTA, basicItemWithoutMeta(Material.MAGENTA_DYE), basicItemWithoutMeta(Material.MAGENTA_BANNER)),
+    LIGHT_BLUE(DyeColor.LIGHT_BLUE, basicItemWithoutMeta(Material.LIGHT_BLUE_DYE), basicItemWithoutMeta(Material.LIGHT_BLUE_BANNER)),
+    YELLOW(DyeColor.YELLOW, basicItemWithoutMeta(Material.YELLOW_DYE), basicItemWithoutMeta(Material.YELLOW_BANNER)),
+    LIME(DyeColor.LIME, basicItemWithoutMeta(Material.LIME_DYE), basicItemWithoutMeta(Material.LIME_BANNER)),
+    PINK(DyeColor.PINK, basicItemWithoutMeta(Material.PINK_DYE), basicItemWithoutMeta(Material.PINK_BANNER)),
+    GRAY(DyeColor.GRAY, basicItemWithoutMeta(Material.GRAY_DYE), basicItemWithoutMeta(Material.GRAY_BANNER)),
+    LIGHT_GRAY(DyeColor.LIGHT_GRAY, basicItemWithoutMeta(Material.LIGHT_GRAY_DYE), basicItemWithoutMeta(Material.LIGHT_GRAY_BANNER)),
+    CYAN(DyeColor.CYAN, basicItemWithoutMeta(Material.CYAN_DYE), basicItemWithoutMeta(Material.CYAN_BANNER)),
+    PURPLE(DyeColor.PURPLE, basicItemWithoutMeta(Material.PURPLE_DYE), basicItemWithoutMeta(Material.PURPLE_BANNER)),
+    BLUE(DyeColor.BLUE, basicItemWithoutMeta(Material.BLUE_DYE), basicItemWithoutMeta(Material.BLUE_BANNER)),
+    BROWN(DyeColor.BROWN, basicItemWithoutMeta(Material.BROWN_DYE), basicItemWithoutMeta(Material.BROWN_BANNER)),
+    GREEN(DyeColor.GREEN, basicItemWithoutMeta(Material.GREEN_DYE), basicItemWithoutMeta(Material.GREEN_BANNER)),
+    RED(DyeColor.RED, basicItemWithoutMeta(Material.RED_DYE), basicItemWithoutMeta(Material.RED_BANNER)),
+    BLACK(DyeColor.BLACK, basicItemWithoutMeta(Material.BLACK_DYE), basicItemWithoutMeta(Material.BLACK_BANNER))
 }
+
+val bannerColors: List<BannerColors>
+    get() = BannerColors.entries.toList()
